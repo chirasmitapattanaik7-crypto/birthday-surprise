10bb45528ebabdfe8c2a56ba00c31562d945d65e
chirasmitapattanaik7-crypto <chirasmitapattanaik7@gmail.com>
2026-06-28 10:45:20 +0530

    Create photos.js

---
+++ photos.js
@@ -0,0 +1,7 @@
+const photos = [
+"https://picsum.photos/600/600?random=1",
+"https://picsum.photos/600/600?random=2",
+"https://picsum.photos/600/600?random=3",
+"https://picsum.photos/600/600?random=4",
+"https://picsum.photos/600/600?random=5"
+];
